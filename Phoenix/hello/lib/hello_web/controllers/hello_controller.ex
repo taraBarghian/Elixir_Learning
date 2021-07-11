2fd316f46ca781f21e9bef7a98d6286c5271e2d1
Tara Barghian <ta94ra12@gmail.com>
2021-07-11 14:23:25 +0430

    up & running first page

---
+++ Phoenix/hello/lib/hello_web/controllers/hello_controller.ex
@@ -0,0 +1,13 @@
+defmodule HelloWeb.HelloController do
+
+  use HelloWeb, :controller
+
+  def index(conn, _params ) do
+    render(conn, "index.html")
+  end
+
+  def show(conn, %{"messenger" => messenger} = params) do
+    render(conn, "show.html", messenger: messenger)
+  end
+
+end
---
+++ Phoenix/hello/lib/hello_web/views/hello_view.ex
@@ -0,0 +1,5 @@
+defmodule HelloWeb.HelloView do
+
+  use HelloWeb, :view
+
+end
