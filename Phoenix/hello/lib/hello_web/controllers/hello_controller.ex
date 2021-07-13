71c24bf6a7f12dbcb0908c9e3e06e830757a9952
Tara Barghian <ta94ra12@gmail.com>
2021-07-13 12:48:39 +0430

    through PLUGS !

--- Phoenix/hello/lib/hello_web/controllers/hello_controller.ex
+++ Phoenix/hello/lib/hello_web/controllers/hello_controller.ex
@@ -2,6 +2,10 @@ defmodule HelloWeb.HelloController do
 
   use HelloWeb, :controller
 
+
+
+  plug HelloWeb.Plugs.Locale, "en" when action in [:index]
+
   def index(conn, _params ) do
     render(conn, "index.html")
   end
