cae6939e9597dc0b8ff303f89d16dbc4c0c9506c
Tara Barghian <ta94ra12@gmail.com>
2021-07-23 21:10:48 +0430

    handling first query

--- Phoenix/discuss/lib/discuss_web/controllers/topic_controller.ex
+++ Phoenix/discuss/lib/discuss_web/controllers/topic_controller.ex
@@ -2,10 +2,21 @@ defmodule DiscussWeb.TopicController do
   use DiscussWeb, :controller
 
   alias DiscussWeb.Topic
+  alias Discuss.Repo
 
-  def new(conn, params) do
+  def new(conn, _params) do
     changeset = Topic.changeset(%Topic{}, %{})
     render conn, "new.html" , changeset: changeset
+  end
+
+  def create(conn, %{"topic" => topic}) do
+   # %{"topic" => topic} = params
+   changeset = Topic.changeset(%Topic{}, topic)
+
+   case Repo.insert(changeset) do
+    {:ok ,post} -> IO.inspect(post)
+    {:error, changeset} -> IO.inspect(changeset)
+   end
   end
 
 end
