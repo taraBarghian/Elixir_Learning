dbe39609b099cb3cc81cab10f1e7eaef9a50dc4d
Tara Barghian <ta94ra12@gmail.com>
2021-07-27 21:22:35 +0430

    dealing with db

--- Phoenix/discuss/lib/discuss_web/controllers/topic_controller.ex
+++ Phoenix/discuss/lib/discuss_web/controllers/topic_controller.ex
@@ -4,6 +4,13 @@ defmodule DiscussWeb.TopicController do
   alias DiscussWeb.Topic
   alias Discuss.Repo
 
+
+  def index(conn, _params) do
+    topics = Repo.all(Topic)
+    render( conn, "index.html" , topics: topics)
+  end
+
+
   def new(conn, _params) do
     changeset = Topic.changeset(%Topic{}, %{})
     render conn, "new.html" , changeset: changeset
@@ -14,9 +21,18 @@ defmodule DiscussWeb.TopicController do
    changeset = Topic.changeset(%Topic{}, topic)
 
    case Repo.insert(changeset) do
-    {:ok ,post} -> IO.inspect(post)
-    {:error, changeset} -> IO.inspect(changeset)
+    {:ok ,post} ->
+      conn
+      |> put_flash(:info, "Topic Created!")
+      |> redirect(to: Routes.topic_path(conn, :index))
+
+    {:error, changeset} ->
+      render conn, "new.html" , changeset: changeset
    end
   end
+
+
+
+
 
 end
