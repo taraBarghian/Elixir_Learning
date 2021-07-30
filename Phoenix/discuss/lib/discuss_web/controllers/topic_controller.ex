77eb339c19ef93a3313d5fb06f5b066488221663
Tara Barghian <ta94ra12@gmail.com>
2021-07-30 11:55:15 +0430

    db features done

--- Phoenix/discuss/lib/discuss_web/controllers/topic_controller.ex
+++ Phoenix/discuss/lib/discuss_web/controllers/topic_controller.ex
@@ -39,10 +39,10 @@ defmodule DiscussWeb.TopicController do
   end
 
   def update(conn, %{"id" => topic_id, "topic" => topic}) do
-    # old_topic = Repo.get(Topic, topic_id)
-    # changeset = Topic.changeset(old_topic,topic)
+    old_topic = Repo.get(Topic, topic_id)
+    changeset = Topic.changeset(old_topic,topic)
 
-    changeset = Repo.get!(Topic, topic_id)|> Topic.changeset(topic)
+    #changeset = Repo.get(Topic, topic_id)|> Topic.changeset(topic)
 
     case Repo.update(changeset) do
        {:ok, _topic} ->
@@ -50,10 +50,20 @@ defmodule DiscussWeb.TopicController do
         |> put_flash(:info , "Topic Updated")
         |> redirect(to: Routes.topic_path(conn, :index))
 
-      {:erros, changeset} ->
-        render conn, "edit.html",changeset
+      {:error, changeset} ->
+        render conn, "edit.html",changeset: changeset , topic: old_topic
 
     end
+  end
+
+  def delete(conn, %{"id" => topic_id}) do
+
+    Repo.get!(Topic, topic_id)
+    |> Repo.delete!()
+
+    conn
+    |> put_flash(:info, "Topic Deleted")
+    |> redirect(to: Routes.topic_path(conn, :index))
 
   end
 
