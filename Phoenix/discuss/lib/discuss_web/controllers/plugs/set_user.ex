aae1cf1a26a4db6267b0330bc526c10569516888
Tara Barghian <ta94ra12@gmail.com>
2021-08-02 21:31:39 +0430

    auth checking by plug

--- Phoenix/discuss/lib/discuss_web/controllers/plugs/set_user.ex
+++ Phoenix/discuss/lib/discuss_web/controllers/plugs/set_user.ex
@@ -7,7 +7,6 @@ defmodule DiscussWeb.Plugs.SetUser do
   alias DiscussWeb.Router.Helpers
 
    def init(_params) do
-
    end
 
    def call(conn, _params) do
--- Phoenix/discuss/lib/discuss_web/controllers/topic_controller.ex
+++ Phoenix/discuss/lib/discuss_web/controllers/topic_controller.ex
@@ -4,6 +4,8 @@ defmodule DiscussWeb.TopicController do
   alias DiscussWeb.Topic
   alias Discuss.Repo
 
+  plug DiscussWeb.Plugs.RequireAuth when action in [:new , :create , :edit , :update, :delete]
+
 
   def index(conn, _params) do
     IO.inspect(conn.assigns)
